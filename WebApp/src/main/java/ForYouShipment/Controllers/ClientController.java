72e7091b55440b12ac0d644721df7641adc94fd5
Emma Andreea Chirlomez <s194780@win.dtu.dk>
2021-04-06 00:12:20 +0300

    some changes

--- WebApp/src/main/java/ForYouShipment/Controllers/ClientController.java
+++ WebApp/src/main/java/ForYouShipment/Controllers/ClientController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import ForYouShipment.Models.ClientProfileModel;
-import ForYouShipment.Models.ClientUserModel;
 import ForYouShipment.Models.UserModel;
 import ForYouShipment.Storage.UserStorage;
 import ForYouShipment.Workers.AuthenticateUserWorker;
